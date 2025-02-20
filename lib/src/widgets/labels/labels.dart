1bad608622b7c112b0a01f75c5761bbbcbedba56
KaanCL <kaancerkez34@outlook.com>
2025-02-20 16:00:28 +0300

    add ✨: RatingText and ProductInfoLabel added

--- lib/src/widgets/labels/labels.dart
+++ lib/src/widgets/labels/labels.dart
@@ -3,3 +3,5 @@ export 'package:shopapp_widgets/src/widgets/labels/slider_indicator/slider_indic
 export 'package:shopapp_widgets/src/widgets/labels/header_image/header_image.dart';
 export 'package:shopapp_widgets/src/widgets/labels/timer_label/timer_label.dart';
 export 'package:shopapp_widgets/src/widgets/labels/timer_label_box/timer_label_box.dart';
+export 'package:shopapp_widgets/src/widgets/labels/product_info_label/product_info_label.dart';
+export 'package:shopapp_widgets/src/widgets/labels/colors_label/colors_label.dart';
--- lib/src/widgets/texts/text.dart
+++ lib/src/widgets/texts/text.dart
@@ -2,3 +2,6 @@ export 'package:shopapp_widgets/src/widgets/texts/onboarding_text/onboarding_tex
 export 'package:shopapp_widgets/src/widgets/texts/head_text/head_text.dart';
 export 'package:shopapp_widgets/src/widgets/texts/product_text/product_text.dart';
 export 'package:shopapp_widgets/src/widgets/texts/homepage_banner_head_text/homepage_banner_head_text.dart';
+export 'package:shopapp_widgets/src/widgets/texts/categorie_banner_text/categorie_banner_text.dart';
+export 'package:shopapp_widgets/src/widgets/texts/section_action_text/section_action_text.dart';
+export 'package:shopapp_widgets/src/widgets/texts/rating_text/rating_text.dart';
