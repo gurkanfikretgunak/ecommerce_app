8193784a84264c5e25422b58e43e949a9b625563
KaanCL <kaancerkez34@outlook.com>
2025-03-05 14:23:26 +0300

    feat 🆕 : Add Google Sign-In authentication with Firebase

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:example/pages/search/search_view.dart';
+import 'package:example/pages/signin/signin_view.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
   OneSignal.initialize(dotenv.env['ONESIGNAL_APP_ID'] ?? "");
   OneSignal.Notifications.requestPermission(true);
@@ -32,6 +35,6 @@ class ExampleApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SearchView());
+        home: SignInView());
   }
 }
---
+++ example/lib/services/auth.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<User?> getCurrentUser() async {
+    try {
+      User? user = auth.currentUser;
+      if (user != null) {
+        return user;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<User?> signUpWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      final UserCredential userCredential =
+          await auth.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
