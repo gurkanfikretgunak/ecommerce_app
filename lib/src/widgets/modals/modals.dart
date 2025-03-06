b3c321cdbc2a200ebd958e90f36097102543b083
KaanCL <kaancerkez34@outlook.com>
2025-03-06 14:40:21 +0300

    add ✨: NotificationBox , NotificationBoxColumn added

--- lib/src/widgets/modals/modals.dart
+++ lib/src/widgets/modals/modals.dart
@@ -1,7 +1,8 @@
+export 'package:shopapp_widgets/src/widgets/modals/address_box_modal/address_box_modal.dart';
 export 'package:shopapp_widgets/src/widgets/modals/image_radius_modal/image_radius_modal.dart';
+export 'package:shopapp_widgets/src/widgets/modals/nofitication_box_Modal/nofitication_box_modal.dart';
+export 'package:shopapp_widgets/src/widgets/modals/payment_card_modal/payment_card_modal.dart';
+export 'package:shopapp_widgets/src/widgets/modals/payment_method_box_modal/payment_method_box_modal.dart';
+export 'package:shopapp_widgets/src/widgets/modals/product_box_modal/product_box_modal.dart';
 export 'package:shopapp_widgets/src/widgets/modals/product_card_modal/product_card_modal.dart';
 export 'package:shopapp_widgets/src/widgets/modals/product_review_modal/product_review_modal.dart';
-export 'package:shopapp_widgets/src/widgets/modals/product_box_modal/product_box_modal.dart';
-export 'package:shopapp_widgets/src/widgets/modals/address_box_modal/address_box_modal.dart';
-export 'package:shopapp_widgets/src/widgets/modals/payment_method_box_modal/payment_method_box_modal.dart';
-export 'package:shopapp_widgets/src/widgets/modals/payment_card_modal/payment_card_modal.dart';
---
+++ lib/src/widgets/modals/nofitication_box_Modal/nofitication_box_modal.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shopapp_widgets/shoapp_ui_kit.dart';
+
+class NofiticationBoxModal extends StatefulWidget {
+  final String iconPath;
+  final String text;
+  final String timeText;
+  final double? radius;
+  final double? width;
+  final double? height;
+  final double? successIconRadius;
+  final Color? successIconColor;
+  final Color? color;
+  final double? fontSize;
+  final double? timeFontSize;
+  final Color? textColor;
+  final Color? timeTextColor;
+  bool isSeen;
+  bool isSelected;
+
+  NofiticationBoxModal({
+    super.key,
+    required this.iconPath,
+    required this.text,
+    required this.timeText,
+    this.radius,
+    this.width,
+    this.height,
+    this.successIconRadius,
+    this.successIconColor,
+    this.color,
+    this.fontSize,
+    this.timeFontSize,
+    this.textColor,
+    this.timeTextColor,
+    required this.isSeen,
+    required this.isSelected,
+  });
+
+  @override
+  State<NofiticationBoxModal> createState() => _NofiticationBoxModalState();
+}
+
+class _NofiticationBoxModalState extends State<NofiticationBoxModal> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SuccessLabelRadius(
+          radius: widget.radius ?? 30,
+          width: widget.width ?? 20,
+          height: widget.height ?? 20,
+          successIconRadius: widget.successIconRadius ?? 5,
+          iconPath: widget.iconPath,
+          successIconColor: widget.isSeen
+              ? ColorConstant.instance.neutral5
+              : widget.successIconColor ?? ColorConstant.instance.primary_main,
+          color: widget.isSeen
+              ? ColorConstant.instance.neutral5
+              : widget.color ?? ColorConstant.instance.primary_main,
+        ),
+        context.emptySizedWidthBoxNormal,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: ProductText(
+                text: widget.text,
+                softWrap: true,
+                textAlign: TextAlign.start,
+                color: widget.isSeen
+                    ? ColorConstant.instance.neutral5
+                    : widget.textColor ?? ColorConstant.instance.neutral1,
+                fontSize: widget.fontSize ?? 12,
+              ),
+            ),
+            ProductText(
+              text: widget.timeText,
+              color: widget.isSeen
+                  ? ColorConstant.instance.neutral5
+                  : widget.timeTextColor ?? ColorConstant.instance.neutral4,
+              fontSize: widget.timeFontSize ?? 10,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
