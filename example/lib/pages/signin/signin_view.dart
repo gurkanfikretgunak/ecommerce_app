cb55cc2911e0e703ba6204955e36d8d02f48f957
KaanCL <kaancerkez34@outlook.com>
2025-03-10 13:50:30 +0300

    refactor ✏️: Restructure sign-in and sign-up handling with AuthCubit

--- example/lib/pages/signin/signin_view.dart
+++ example/lib/pages/signin/signin_view.dart
@@ -1,110 +1,100 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:example/cubits/auth/auth_state.dart';
 import 'package:example/gen/assets.gen.dart';
 import 'package:example/route/route.gr.dart';
-import 'package:example/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shopapp_widgets/shoapp_ui_kit.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:example/cubits/auth/auth_cubit.dart';
 
 @RoutePage()
-class SignInView extends StatefulWidget {
+class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
-  State<SignInView> createState() => _SignInViewState();
-}
-
-class _SignInViewState extends State<SignInView> {
-  Future<User?>? _signInFuture;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SignInHeader(
-                imagePath: Assets.images.signinheader.path,
-                onPressed: () {
-                  AutoRouter.of(context).push(const OnboardingViewRoute());
-                }),
-            context.emptySizedHeightBoxNormal,
-            const SignInForm(),
-            context.emptySizedHeightBoxNormal,
-            Column(
-              children: [
-                CustomButton(
-                  text: "SIGN IN",
-                  onPressed: () {},
-                ),
-                context.emptySizedHeightBoxLow,
-                CustomButton(
-                  icon: FontAwesomeIcons.google,
-                  text: "SIGN IN WITH GOOGLE",
-                  color: ColorConstant.instance.neutral9,
-                  textColor: ColorConstant.instance.neutral1,
-                  iconColor: ColorConstant.instance.neutral1,
-                  onPressed: () {
-                    setState(() {
-                      _signInFuture = AuthService().signUpWithGoogle();
-                    });
-                  },
-                ),
-                context.emptySizedHeightBoxLow,
-                CustomButton(
-                  icon: FontAwesomeIcons.facebook,
-                  text: "SIGN IN WITH FACEBOOK",
-                  color: ColorConstant.instance.neutral9,
-                  textColor: ColorConstant.instance.neutral1,
-                  iconColor: ColorConstant.instance.neutral1,
-                  onPressed: () {},
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don’t have an account?"),
-                    TextButton(
-                        onPressed: () {
-                          AutoRouter.of(context).push(SignUpViewRoute());
-                          /* Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUpView()));*/
-                        },
-                        child: Text(
-                          "Register",
-                          style: TextStyle(
-                            color: ColorConstant.instance.primary_main,
-                          ),
-                        ))
-                  ],
-                ),
-                if (_signInFuture != null)
-                  FutureBuilder<User?>(
-                    future: _signInFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        if (snapshot.data != null) {
-                          WidgetsBinding.instance.addPostFrameCallback((_) {
-                            AutoRouter.of(context).push(
-                                MainpageViewRoute()); /*Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const MainpageView()));*/
-                          });
-                        } else {
-                          return Text('Sign in failed');
-                        }
-                      }
-                      return Container();
+    return BlocProvider(
+      create: (_) => AuthCubit(),
+      child: Scaffold(
+        body: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthAuthenticated) {
+              AutoRouter.of(context).push(MainpageViewRoute());
+            } else if (state is AuthError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.error)),
+              );
+            }
+          },
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SignInHeader(
+                    imagePath: Assets.images.signinheader.path,
+                    onPressed: () {
+                      AutoRouter.of(context).push(const OnboardingViewRoute());
                     },
                   ),
-              ],
-            ),
-          ],
+                  context.emptySizedHeightBoxNormal,
+                  const SignInForm(),
+                  context.emptySizedHeightBoxNormal,
+                  Column(
+                    children: [
+                      CustomButton(
+                        text: "SIGN IN",
+                        onPressed: () {},
+                      ),
+                      context.emptySizedHeightBoxLow,
+                      CustomButton(
+                        icon: FontAwesomeIcons.google,
+                        text: "SIGN IN WITH GOOGLE",
+                        color: ColorConstant.instance.neutral9,
+                        textColor: ColorConstant.instance.neutral1,
+                        iconColor: ColorConstant.instance.neutral1,
+                        onPressed: () {
+                          context.read<AuthCubit>().signUpWithGoogle();
+                        },
+                      ),
+                      context.emptySizedHeightBoxLow,
+                      CustomButton(
+                        icon: FontAwesomeIcons.facebook,
+                        text: "SIGN IN WITH FACEBOOK",
+                        color: ColorConstant.instance.neutral9,
+                        textColor: ColorConstant.instance.neutral1,
+                        iconColor: ColorConstant.instance.neutral1,
+                        onPressed: () {},
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don’t have an account?"),
+                          TextButton(
+                            onPressed: () {
+                              AutoRouter.of(context)
+                                  .push(const SignUpViewRoute());
+                            },
+                            child: Text(
+                              "Register",
+                              style: TextStyle(
+                                color: ColorConstant.instance.primary_main,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (state is AuthLoading)
+                        const CircularProgressIndicator(),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
--- example/lib/pages/signup/signup_view.dart
+++ example/lib/pages/signup/signup_view.dart
@@ -1,13 +1,15 @@
-import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:example/pages/mainpage/mainpage_view.dart';
-import 'package:example/pages/verifiticion/verification_view.dart';
+import 'package:example/cubits/auth/auth_state.dart';
 import 'package:example/route/route.gr.dart';
 import 'package:example/services/auth.dart';
+// ignore: depend_on_referenced_packages
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shopapp_widgets/shoapp_ui_kit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:example/cubits/auth/auth_cubit.dart';
 
 @RoutePage()
 class SignUpView extends StatefulWidget {
@@ -23,67 +25,81 @@ class _SignUpViewState extends State<SignUpView> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeigth = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(60),
-          child: CustomAppbar(
-            text: "CREATE YOUR ACCOUNT",
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            iconColor: ColorConstant.instance.neutral1,
-          )),
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.only(left: 20, top: screenHeigth * 0.03)),
-          const SignUpForm(),
-          CustomButton(
-            text: "SIGN UP",
-            onPressed: () {
-              AutoRouter.of(context).push(const VerificationViewRoute());
-              /*Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const VerificationView())))*/
-            },
-          ),
-          context.emptySizedHeightBoxLow,
-          CustomButton(
-            icon: FontAwesomeIcons.google,
-            text: "SIGN UP WITH GOOGLE",
-            color: ColorConstant.instance.neutral9,
-            textColor: ColorConstant.instance.neutral1,
-            iconColor: ColorConstant.instance.neutral1,
-            onPressed: () {
-              setState(() {
-                _signUpFuture = AuthService().signUpWithGoogle();
-              });
-            },
-          ),
-          context.emptySizedHeightBoxLow,
-          CustomButton(
-            icon: FontAwesomeIcons.facebook,
-            text: "SIGN UP WITH FACEBOOK",
-            color: ColorConstant.instance.neutral9,
-            textColor: ColorConstant.instance.neutral1,
-            iconColor: ColorConstant.instance.neutral1,
-            onPressed: () {},
-          ),
-          if (_signUpFuture != null)
-            FutureBuilder<User?>(
-                future: _signUpFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    if (snapshot.data != null) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        AutoRouter.of(context)
-                            .replaceAll([MainpageViewRoute()]);
-                      });
-                    }
-                  }
-                  return Container();
-                })
-        ],
+    return BlocProvider(
+      create: (_) => AuthCubit(),
+      child: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: CustomAppbar(
+              text: "CREATE YOUR ACCOUNT",
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              iconColor: ColorConstant.instance.neutral1,
+            )),
+        body: BlocConsumer<AuthCubit, AuthState>(listener: (context, state) {
+          if (state is AuthAuthenticated) {
+            AutoRouter.of(context).push(MainpageViewRoute());
+          } else if (state is AuthError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        }, builder: (context, state) {
+          return Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(left: 20, top: screenHeigth * 0.03)),
+              const SignUpForm(),
+              CustomButton(
+                text: "SIGN UP",
+                onPressed: () {
+                  AutoRouter.of(context).push(const VerificationViewRoute());
+                  /*Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const VerificationView())))*/
+                },
+              ),
+              context.emptySizedHeightBoxLow,
+              CustomButton(
+                icon: FontAwesomeIcons.google,
+                text: "SIGN UP WITH GOOGLE",
+                color: ColorConstant.instance.neutral9,
+                textColor: ColorConstant.instance.neutral1,
+                iconColor: ColorConstant.instance.neutral1,
+                onPressed: () {
+                  setState(() {
+                    _signUpFuture = AuthService().signUpWithGoogle();
+                  });
+                },
+              ),
+              context.emptySizedHeightBoxLow,
+              CustomButton(
+                icon: FontAwesomeIcons.facebook,
+                text: "SIGN UP WITH FACEBOOK",
+                color: ColorConstant.instance.neutral9,
+                textColor: ColorConstant.instance.neutral1,
+                iconColor: ColorConstant.instance.neutral1,
+                onPressed: () {},
+              ),
+              if (_signUpFuture != null)
+                FutureBuilder<User?>(
+                    future: _signUpFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data != null) {
+                          WidgetsBinding.instance.addPostFrameCallback((_) {
+                            AutoRouter.of(context)
+                                .replaceAll([MainpageViewRoute()]);
+                          });
+                        }
+                      }
+                      return Container();
+                    })
+            ],
+          );
+        }),
       ),
     );
   }
