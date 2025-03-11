74b970dc45d584af41625919641afaa5f1fe0b19
KaanCL <kaancerkez34@outlook.com>
2025-03-11 16:03:43 +0300

    refactor ✏️: Manage data in ProductView using Product Cubit

--- example/lib/pages/product/product_view.dart
+++ example/lib/pages/product/product_view.dart
@@ -1,11 +1,11 @@
-import 'package:example/gen/assets.gen.dart';
-import 'package:example/pages/mainpage/mainpage_view.dart';
-import 'package:example/pages/payment/payment_view.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:example/cubits/product/product_cubit.dart';
+import 'package:example/cubits/product/product_state.dart';
 import 'package:example/route/route.gr.dart';
 import 'package:flutter/material.dart';
-import 'package:shopapp_widgets/shoapp_ui_kit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:auto_route/auto_route.dart';
+import 'package:shopapp_widgets/shoapp_ui_kit.dart';
 
 @RoutePage()
 class ProductView extends StatefulWidget {
@@ -16,54 +16,11 @@ class ProductView extends StatefulWidget {
 }
 
 class _ProductViewState extends State<ProductView> {
+  @override
   void initState() {
     super.initState();
+    context.read<ProductCubit>().loadProductData();
   }
-
-  final List<String> imagePaths = [
-    Assets.images.productheaderimageFirst.path,
-    Assets.images.productheaderimageFirst.path,
-    Assets.images.productheaderimageFirst.path,
-  ];
-  List<Widget> productCardItems = [
-    ProductCardModal(
-        imagePath: Assets.images.productcardimageFirst.path,
-        productStock: "Sold(50 Product)",
-        productName: "Long-sleeved T-shirt",
-        productPrice: "\$49.00"),
-    ProductCardModal(
-        imagePath: Assets.images.productcardimageSecond.path,
-        productStock: "Sold(50 Product)",
-        productName: "Printed Cotton Shirt",
-        productPrice: "\$45.00"),
-    ProductCardModal(
-        imagePath: Assets.images.productcardimageThird.path,
-        productStock: "Sold(50 Product)",
-        productName: "Cotton T-shirt",
-        productPrice: "\$49.00"),
-    ProductCardModal(
-        imagePath: Assets.images.productcardimageFourth.path,
-        productStock: "Sold(50 Product)",
-        productName: "Embroidered T-Shirt",
-        productPrice: "\$39.00"),
-  ];
-
-  final List<Widget> reviewItems = [
-    const ProductReviewModal(
-        name: "Theresa Webb",
-        rate: 5,
-        date: "May1,2022",
-        reviewText:
-            "Cursus sit amet dictum sit amet justo donec enim. Commodo ullamcorper a lacus",
-        imagePath: "assets/images/categorierow_first.png"),
-    const ProductReviewModal(
-        name: "Theresa Webb",
-        rate: 3,
-        date: "May1,2022",
-        reviewText:
-            "Cursus sit amet dictum sit amet justo donec enim. Commodo ullamcorper a lacus",
-        imagePath: "assets/images/categorierow_first.png"),
-  ];
 
   void _shopPopup() {
     showDialog(
@@ -73,12 +30,6 @@ class _ProductViewState extends State<ProductView> {
             title: "SUCCESS",
             buttonOnPressed: () {
               AutoRouter.of(context).push(MainpageViewRoute(pageNo: 2));
-              /*Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MainpageView(
-                            pageNo: 2,
-                          )));*/
             },
             description:
                 "Dolor magna eget est lorem ipsum dolor sit amet consectetur.",
@@ -91,78 +42,91 @@ class _ProductViewState extends State<ProductView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ProductHeader(
-              imagePaths: imagePaths,
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              action: [
-                InkWell(
-                  onTap: () {
-                    AutoRouter.of(context).push(const PaymentViewRoute());
-                  },
-                  child: SvgPicture.asset("assets/icons/cart.svg"),
-                ),
-                context.emptySizedWidthBoxNormal,
-                InkWell(
-                  onTap: () {
-                    AutoRouter.of(context).push(const SearchViewRoute());
-                  },
-                  child: SvgPicture.asset("assets/icons/search.svg"),
-                ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15),
-              child: ProductInfoLabel(
-                rating: "4,9",
-                reviewCount: "12",
-                soldCount: "50",
-                productName: "Printed cotton shirt",
-                description:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus urna duis convallis convallis tellus.",
+      body: BlocBuilder<ProductCubit, ProductState>(
+        builder: (context, state) {
+          if (state is ProductLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is ProductLoaded) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  ProductHeader(
+                    imagePaths: state.imagePaths,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    action: [
+                      InkWell(
+                        onTap: () {
+                          AutoRouter.of(context).push(const PaymentViewRoute());
+                        },
+                        child: SvgPicture.asset("assets/icons/cart.svg"),
+                      ),
+                      context.emptySizedWidthBoxNormal,
+                      InkWell(
+                        onTap: () {
+                          AutoRouter.of(context).push(const SearchViewRoute());
+                        },
+                        child: SvgPicture.asset("assets/icons/search.svg"),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ProductInfoLabel(
+                      rating: state.rating,
+                      reviewCount: state.reviewCount,
+                      soldCount: state.soldCount,
+                      productName: state.productName,
+                      description: state.description,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ProductSectionLabel(
+                        title: "Color:",
+                        element: ColorsLabel(colors: state.colors)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ProductSectionLabel(
+                        title: "Size:",
+                        element: SizesLabel(sizes: state.sizes)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ProductDescriptionLabel(
+                      text: state.productDescriptionText,
+                      imagePath: state.productDescriptionImagePath,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ProductReviewColumnLayout(
+                      items: state.reviewItems,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(15),
+                    child: ReviewFormLabel(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: SectionLayout(
+                      sectionText: "RELATED PRODUCT",
+                      layout: ProductRowLayout(items: state.productCardItems),
+                    ),
+                  ),
+                  context.emptySizedHeightBoxHigh,
+                ],
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15),
-              child:
-                  ProductSectionLabel(title: "Color:", element: ColorsLabel()),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15),
-              child: ProductSectionLabel(title: "Size:", element: SizesLabel()),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15),
-              child: ProductDescriptionLabel(
-                text:
-                    "Purus in massa tempor nec feugiat. Congue nisi vitae suscipit tellus mauris a diam. Nam aliquam sem et tortor. Quis risus sed vulputate odio ut enim. Ultrices dui sapien eget mi proin sed libero enim sed. Quis viverra nibh cras pulvinar mattis nunc sed blandit libero. At volutpat diam ut venenatis tellus in",
-                imagePath: "assets/images/productdescriptionimage.png",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: ProductReviewColumnLayout(
-                items: reviewItems,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15),
-              child: ReviewFormLabel(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: SectionLayout(
-                sectionText: "RELATED PRODUCT",
-                layout: ProductRowLayout(items: productCardItems),
-              ),
-            ),
-            context.emptySizedHeightBoxHigh,
-          ],
-        ),
+            );
+          } else if (state is ProductError) {
+            return Center(child: Text('Error: ${state.message}'));
+          } else {
+            return Container();
+          }
+        },
       ),
       bottomSheet: ProductBottomSheetLabel(
         buttonOnPressed: _shopPopup,
