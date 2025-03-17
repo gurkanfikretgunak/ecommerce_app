42c0b7a1eff0ae8314fd0b0439b564529586d5d0
KaanCL <kaancerkez34@outlook.com>
2025-03-17 16:50:06 +0300

    feat 🆕: Added validation states for first name, last name, phone, and confirm password

--- example/lib/cubits/validation/validation_cubit.dart
+++ example/lib/cubits/validation/validation_cubit.dart
@@ -6,6 +6,10 @@ class ValidationCubit extends Cubit<ValidationState> {
 
   bool _isEmailValid = false;
   bool _isPasswordValid = false;
+  bool _isFirstNameValid = false;
+  bool _isLastNameValid = false;
+  bool _isPhoneValid = false;
+  bool _isConfirmPasswordValid = false;
 
   void validateEmail(String email) {
     if (email.isEmpty) {
@@ -39,7 +43,69 @@ class ValidationCubit extends Cubit<ValidationState> {
     }
   }
 
-  bool isFormValid() {
+  void validateFirstName(String firstName) {
+    if (firstName.isEmpty) {
+      _isFirstNameValid = false;
+      emit(FirstNameInvalid('First name cannot be empty'));
+    } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(firstName)) {
+      _isFirstNameValid = false;
+      emit(FirstNameInvalid(
+          'First name cannot contain special characters or numbers'));
+    } else {
+      _isFirstNameValid = true;
+      emit(FirstNameValid());
+    }
+  }
+
+  void validateLastName(String lastName) {
+    if (lastName.isEmpty) {
+      _isLastNameValid = false;
+      emit(LastNameInvalid('Last name cannot be empty'));
+    } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(lastName)) {
+      _isLastNameValid = false;
+      emit(LastNameInvalid(
+          'Last name cannot contain special characters or numbers'));
+    } else {
+      _isLastNameValid = true;
+      emit(LastNameValid());
+    }
+  }
+
+  void validatePhone(String phone) {
+    if (phone.isEmpty) {
+      _isPhoneValid = false;
+      emit(PhoneInvalid('Phone number cannot be empty'));
+    } else if (!RegExp(r'^\+?[1-9]\d{1,14}$').hasMatch(phone)) {
+      _isPhoneValid = false;
+      emit(PhoneInvalid('Invalid phone number format'));
+    } else {
+      _isPhoneValid = true;
+      emit(PhoneValid());
+    }
+  }
+
+  void validateConfirmPassword(String confirmPassword, String password) {
+    if (confirmPassword.isEmpty) {
+      _isConfirmPasswordValid = false;
+      emit(ConfirmPasswordInvalid('Confirm password cannot be empty'));
+    } else if (confirmPassword != password) {
+      _isConfirmPasswordValid = false;
+      emit(ConfirmPasswordInvalid('Passwords do not match'));
+    } else {
+      _isConfirmPasswordValid = true;
+      emit(ConfirmPasswordValid());
+    }
+  }
+
+  bool isSignInFormValid() {
     return _isEmailValid && _isPasswordValid;
+  }
+
+  bool isSignUpFormValid() {
+    return _isFirstNameValid &&
+        _isLastNameValid &&
+        _isEmailValid &&
+        _isPasswordValid &&
+        _isConfirmPasswordValid;
   }
 }
--- example/lib/cubits/validation/validation_state.dart
+++ example/lib/cubits/validation/validation_state.dart
@@ -29,12 +29,58 @@ class PasswordInvalid extends ValidationState {
   List<Object?> get props => [error];
 }
 
+class FirstNameValid extends ValidationState {}
+
+class FirstNameInvalid extends ValidationState {
+  final String error;
+
+  FirstNameInvalid(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
+
+class LastNameValid extends ValidationState {}
+
+class LastNameInvalid extends ValidationState {
+  final String error;
+
+  LastNameInvalid(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
+
+class PhoneValid extends ValidationState {}
+
+class PhoneInvalid extends ValidationState {
+  final String error;
+
+  PhoneInvalid(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
+
+class ConfirmPasswordValid extends ValidationState {}
+
+class ConfirmPasswordInvalid extends ValidationState {
+  final String error;
+
+  ConfirmPasswordInvalid(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
+
 class ValidationSuccess extends ValidationState {
   final bool isEmailValid;
   final bool isPasswordValid;
 
-  ValidationSuccess(
-      {required this.isEmailValid, required this.isPasswordValid});
+  ValidationSuccess({
+    required this.isEmailValid,
+    required this.isPasswordValid,
+  });
 
   @override
   List<Object?> get props => [isEmailValid, isPasswordValid];
