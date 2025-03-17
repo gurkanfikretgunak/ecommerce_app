e9035b7f56c0f61280ebb5cd96a1d37e8d3d2286
KaanCL <kaancerkez34@outlook.com>
2025-03-17 16:51:03 +0300

    feat 🆕: added form validation to SignUpView

--- example/lib/pages/signup/signup_view.dart
+++ example/lib/pages/signup/signup_view.dart
@@ -1,16 +1,15 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:example/cubits/auth/auth_cubit.dart';
 import 'package:example/cubits/auth/auth_state.dart';
+import 'package:example/cubits/validation/validation_cubit.dart';
+import 'package:example/cubits/validation/validation_state.dart';
 import 'package:example/route/route.gr.dart';
 import 'package:example/services/auth/auth_service.dart';
-// ignore: depend_on_referenced_packages
-//import 'package:firebase_auth/firebase_auth.dart';
-import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shopapp_widgets/shoapp_ui_kit.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:example/cubits/auth/auth_cubit.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
 @RoutePage()
 class SignUpView extends StatefulWidget {
@@ -21,8 +20,6 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
-  Future<supabase.User?>? _signUpFuture;
-
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
@@ -33,96 +30,162 @@ class _SignUpViewState extends State<SignUpView> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeigth = MediaQuery.of(context).size.height;
-    return BlocProvider(
-        create: (_) => AuthCubit(),
-        child: Scaffold(
-            appBar: PreferredSize(
-                preferredSize: const Size.fromHeight(60),
-                child: CustomAppbar(
-                  text: "CREATE YOUR ACCOUNT",
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  iconColor: ColorConstant.instance.neutral1,
-                )),
-            body: BlocConsumer<AuthCubit, AuthState>(
-              listener: (context, state) {
-                if (state is AuthAuthenticated) {
-                  AutoRouter.of(context).push(MainpageViewRoute());
-                } else if (state is AuthSignUpSuccess) {
-                  AutoRouter.of(context).push(SignInViewRoute());
-                } else if (state is AuthError) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(state.error)),
-                  );
-                }
-              },
-              builder: (context, state) {
-                return SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                            left: 20,
-                            top: MediaQuery.of(context).size.height * 0.03),
-                      ),
-                      SignUpForm(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+        BlocProvider(create: (_) => ValidationCubit()),
+      ],
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: CustomAppbar(
+            text: "CREATE YOUR ACCOUNT",
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            iconColor: ColorConstant.instance.neutral1,
+          ),
+        ),
+        body: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthAuthenticated) {
+              AutoRouter.of(context).push(MainpageViewRoute());
+            } else if (state is AuthSignUpSuccess) {
+              AutoRouter.of(context).push(SignInViewRoute());
+            } else if (state is AuthError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.error)),
+              );
+            }
+          },
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 20,
+                      top: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                  ),
+                  BlocBuilder<ValidationCubit, ValidationState>(
+                    builder: (context, validationState) {
+                      bool isFirstNameValid = true;
+                      bool isLastNameValid = true;
+                      bool isEmailValid = true;
+                      bool isPhoneValid = true;
+                      bool isPasswordValid = true;
+                      bool isConfirmPasswordValid = true;
+
+                      if (validationState is FirstNameInvalid) {
+                        isFirstNameValid = false;
+                      }
+                      if (validationState is LastNameInvalid) {
+                        isLastNameValid = false;
+                      }
+                      if (validationState is EmailInvalid) {
+                        isEmailValid = false;
+                      }
+
+                      if (validationState is PhoneInvalid) {
+                        isPasswordValid = false;
+                      }
+
+                      if (validationState is PasswordInvalid) {
+                        isPasswordValid = false;
+                      }
+
+                      if (validationState is ConfirmPasswordInvalid) {
+                        isConfirmPasswordValid = false;
+                      }
+
+                      return SignUpForm(
                         firstNameController: firstNameController,
                         lastNameController: lastNameController,
                         emailController: emailController,
                         phoneController: phoneController,
                         passwordController: passwordController,
                         confirmPasswordController: confirmPasswordController,
-                      ),
-                      CustomButton(
-                        text: "SIGN UP",
-                        onPressed: () {
-                          context.read<AuthCubit>().signUp(
-                              emailController.text, passwordController.text);
+                        onChangedFirstName: (value) {
+                          context
+                              .read<ValidationCubit>()
+                              .validateFirstName(value);
                         },
-                      ),
-                      context.emptySizedHeightBoxLow,
-                      CustomButton(
-                        icon: FontAwesomeIcons.google,
-                        text: "SIGN UP WITH GOOGLE",
-                        color: ColorConstant.instance.neutral9,
-                        textColor: ColorConstant.instance.neutral1,
-                        iconColor: ColorConstant.instance.neutral1,
-                        onPressed: () {
-                          setState(() {
-                            _signUpFuture = AuthService().signUpWithGoogle();
-                          });
+                        onChangedLastName: (value) {
+                          context
+                              .read<ValidationCubit>()
+                              .validateLastName(value);
                         },
-                      ),
-                      context.emptySizedHeightBoxLow,
-                      CustomButton(
-                        icon: FontAwesomeIcons.facebook,
-                        text: "SIGN UP WITH FACEBOOK",
-                        color: ColorConstant.instance.neutral9,
-                        textColor: ColorConstant.instance.neutral1,
-                        iconColor: ColorConstant.instance.neutral1,
-                        onPressed: () {},
-                      ),
-                      if (_signUpFuture != null)
-                        FutureBuilder<supabase.User?>(
-                          future: _signUpFuture,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData && snapshot.data != null) {
-                              WidgetsBinding.instance.addPostFrameCallback((_) {
-                                AutoRouter.of(context)
-                                    .replaceAll([MainpageViewRoute()]);
-                              });
-                            }
-                            return Container();
-                          },
-                        )
-                    ],
+                        onChangedEmail: (value) {
+                          context.read<ValidationCubit>().validateEmail(value);
+                        },
+                        onChangedPhone: (value) {
+                          context.read<ValidationCubit>().validatePhone(value);
+                        },
+                        onChangedPassword: (value) {
+                          context
+                              .read<ValidationCubit>()
+                              .validatePassword(value);
+                        },
+                        onChangedConfirmPassword: (value) {
+                          context
+                              .read<ValidationCubit>()
+                              .validateConfirmPassword(
+                                  value, passwordController.text);
+                        },
+                        isFirstNameValid: isFirstNameValid,
+                        isLastNameValid: isLastNameValid,
+                        isEmailValid: isEmailValid,
+                        isPhoneValid: isPhoneValid,
+                        isPasswordValid: isPasswordValid,
+                        isConfirmPasswordValid: isConfirmPasswordValid,
+                      );
+                    },
                   ),
-                );
-              },
-            )));
+                  CustomButton(
+                    text: "SIGN UP",
+                    onPressed: () {
+                      if (context.read<ValidationCubit>().isSignUpFormValid()) {
+                        context.read<AuthCubit>().signUp(
+                              emailController.text,
+                              passwordController.text,
+                            );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content:
+                                  Text('Please fix the errors in the form')),
+                        );
+                      }
+                    },
+                  ),
+                  context.emptySizedHeightBoxLow,
+                  CustomButton(
+                    icon: FontAwesomeIcons.google,
+                    text: "SIGN UP WITH GOOGLE",
+                    color: ColorConstant.instance.neutral9,
+                    textColor: ColorConstant.instance.neutral1,
+                    iconColor: ColorConstant.instance.neutral1,
+                    onPressed: () {
+                      AuthService().signUpWithGoogle();
+                    },
+                  ),
+                  context.emptySizedHeightBoxLow,
+                  CustomButton(
+                    icon: FontAwesomeIcons.facebook,
+                    text: "SIGN UP WITH FACEBOOK",
+                    color: ColorConstant.instance.neutral9,
+                    textColor: ColorConstant.instance.neutral1,
+                    iconColor: ColorConstant.instance.neutral1,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/src/widgets/labels/signup_form_label/signup_form_label.dart
+++ lib/src/widgets/labels/signup_form_label/signup_form_label.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:shopapp_widgets/shoapp_ui_kit.dart';
 
 class SignUpForm extends StatefulWidget {
-  // Controller'ları SignUpForm widget'ına parametre olarak alıyoruz
   final TextEditingController firstNameController;
   final TextEditingController lastNameController;
   final TextEditingController emailController;
   final TextEditingController phoneController;
   final TextEditingController passwordController;
   final TextEditingController confirmPasswordController;
+  final Function(String)? onChangedFirstName;
+  final Function(String)? onChangedLastName;
+  final Function(String)? onChangedEmail;
+  final Function(String)? onChangedPhone;
+  final Function(String)? onChangedPassword;
+  final Function(String)? onChangedConfirmPassword;
+  final bool? isFirstNameValid;
+  final bool? isLastNameValid;
+  final bool? isEmailValid;
+  final bool? isPhoneValid;
+  final bool? isPasswordValid;
+  final bool? isConfirmPasswordValid;
 
   const SignUpForm({
     super.key,
@@ -18,6 +29,18 @@ class SignUpForm extends StatefulWidget {
     required this.phoneController,
     required this.passwordController,
     required this.confirmPasswordController,
+    this.onChangedFirstName,
+    this.onChangedLastName,
+    this.onChangedEmail,
+    this.onChangedPhone,
+    this.onChangedPassword,
+    this.onChangedConfirmPassword,
+    this.isFirstNameValid,
+    this.isLastNameValid,
+    this.isEmailValid,
+    this.isPhoneValid,
+    this.isPasswordValid,
+    this.isConfirmPasswordValid,
   });
 
   @override
@@ -40,6 +63,8 @@ class _SignUpFormState extends State<SignUpForm> {
                   child: TextFieldInput(
                     controller: widget.firstNameController,
                     hintText: 'First Name',
+                    onChanged: widget.onChangedFirstName,
+                    isValid: widget.isFirstNameValid ?? false,
                   ),
                 ),
                 context.emptySizedWidthBoxNormal,
@@ -47,6 +72,8 @@ class _SignUpFormState extends State<SignUpForm> {
                   child: TextFieldInput(
                     controller: widget.lastNameController,
                     hintText: 'Last Name',
+                    onChanged: widget.onChangedLastName,
+                    isValid: widget.isLastNameValid ?? false,
                   ),
                 ),
               ],
@@ -56,24 +83,32 @@ class _SignUpFormState extends State<SignUpForm> {
               controller: widget.emailController,
               hintText: 'Email',
               inputType: InputType.email,
+              onChanged: widget.onChangedEmail,
+              isValid: widget.isEmailValid ?? false,
             ),
             context.emptySizedHeightBoxNormal,
             TextFieldInput(
               controller: widget.phoneController,
               hintText: 'Phone Number',
               inputType: InputType.phone,
+              onChanged: widget.onChangedPhone,
+              isValid: widget.isPhoneValid ?? false,
             ),
             context.emptySizedHeightBoxNormal,
             TextFieldInput(
               controller: widget.passwordController,
               hintText: 'Password',
               inputType: InputType.password,
+              onChanged: widget.onChangedPassword,
+              isValid: widget.isPasswordValid ?? false,
             ),
             context.emptySizedHeightBoxNormal,
             TextFieldInput(
               controller: widget.confirmPasswordController,
               hintText: 'Confirm Password',
               inputType: InputType.password,
+              onChanged: widget.onChangedConfirmPassword,
+              isValid: widget.isConfirmPasswordValid ?? false,
             ),
             context.emptySizedHeightBoxNormal,
           ],
