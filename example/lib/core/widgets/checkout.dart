fdc9ac86028ff7c4da62cbaaf4c3c38ad4c0dd4d
KaanCL <kaancerkez34@outlook.com>
2025-03-27 15:17:22 +0300

    refactor ✏️: Checkout view productbox now pulls data from CartCubit

--- example/lib/core/widgets/checkout.dart
+++ example/lib/core/widgets/checkout.dart
@@ -1,10 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:example/core/gen/assets.gen.dart';
-import 'package:example/views/addresses/addresses_view.dart';
-import 'package:example/views/paymentmethods/paymentmethods_view.dart';
+import 'package:example/cubits/product/product_cubit.dart';
 import 'package:example/route/route.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:shopapp_widgets/shoapp_ui_kit.dart';
-import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:example/cubits/cart/cart_cubit.dart';
+import 'package:example/cubits/cart/cart_state.dart';
 
 @RoutePage()
 class Checkout extends StatefulWidget {
@@ -16,57 +18,72 @@ class Checkout extends StatefulWidget {
 }
 
 class _CheckoutState extends State<Checkout> {
-  final List<ProductBoxModal> productBoxRowItems = [
-    ProductBoxModal(
-        imagePath: Assets.images.productboximage.path,
-        name: "Basic T-shirt",
-        price: 49.99),
-    ProductBoxModal(
-        imagePath: Assets.images.productboximage.path,
-        name: "Basic T-shirt",
-        price: 40.99),
-    ProductBoxModal(
-        imagePath: Assets.images.productboximage.path,
-        name: "Basic T-shirt",
-        price: 52.99),
-  ];
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          context.emptySizedHeightBoxNormal,
-          AddressBoxModal(
-              name: "Theresa Webb",
-              address: "3517 W. Gray St. Utica, Pennsylvania 57867",
-              email: "willie.jennings@example.com",
-              onTap: () {
-                AutoRouter.of(context).push(AddressesViewRoute());
-                /*Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AddressesView()));*/
-              },
-              phone: "(480) 555-0103"),
-          context.emptySizedHeightBoxNormal,
-          PaymentMethodBoxModal(
-              onTap: () {
-                AutoRouter.of(context).push(const PaymentMethodsViewRoute());
-                /*Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const PaymentMethodsView()));*/
-              },
-              text: "Master card Ending *****23"),
-          context.emptySizedHeightBoxNormal,
-          CheckoutListLayout(items: productBoxRowItems),
-          context.emptySizedHeightBoxNormal,
-          CustomButton(
-              onPressed: widget.buttonCallBack ?? () {},
-              height: 50,
-              text: "Order Now"),
-        ],
-      ),
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, cartState) {
+        Widget checkoutContent;
+
+        if (cartState is CartLoading) {
+          checkoutContent = const Center(child: CircularProgressIndicator());
+        } else if (cartState is CartLoaded) {
+          checkoutContent = Column(
+            children: [
+              context.emptySizedHeightBoxNormal,
+              AddressBoxModal(
+                name: "Theresa Webb",
+                address: "3517 W. Gray St. Utica, Pennsylvania 57867",
+                email: "willie.jennings@example.com",
+                onTap: () {
+                  AutoRouter.of(context).push(AddressesViewRoute());
+                },
+                phone: "(480) 555-0103",
+              ),
+              context.emptySizedHeightBoxNormal,
+              PaymentMethodBoxModal(
+                onTap: () {
+                  AutoRouter.of(context).push(const PaymentMethodsViewRoute());
+                },
+                text: "Master card Ending *****23",
+              ),
+              context.emptySizedHeightBoxNormal,
+              CheckoutListLayout(
+                items: cartState.cart.map((item) {
+                  return ProductBoxModal(
+                    imagePath: item.product!.image,
+                    name: item.product!.name,
+                    price: item.product!.price,
+                    quantity: item.quantity,
+                    size: item.size,
+                    onTap: () {
+                      context.read<ProductCubit>().changeProduct(item.product!);
+                      AutoRouter.of(context).push(const ProductViewRoute());
+                    },
+                  );
+                }).toList(),
+              ),
+              context.emptySizedHeightBoxNormal,
+              CustomButton(
+                onPressed: widget.buttonCallBack ?? () {},
+                height: 50,
+                text: "Order Now",
+              ),
+            ],
+          );
+        } else {
+          checkoutContent = SizedBox(
+            height: 100,
+            child: Center(
+              child: HeadText(
+                text: "Cart is Empty",
+                color: ColorConstant.instance.neutral1,
+              ),
+            ),
+          );
+        }
+
+        return SingleChildScrollView(child: checkoutContent);
+      },
     );
   }
 }
