7f2dc757ed2b76221871959752db07bb8ce14a3e
KaanCL <kaancerkez34@outlook.com>
2025-03-28 14:36:37 +0300

    feat 🆕: BillingDetailCubit integrated into Checkout view and data provided to AddressBoxModal

--- example/lib/core/widgets/checkout.dart
+++ example/lib/core/widgets/checkout.dart
@@ -2,6 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:example/core/gen/assets.gen.dart';
 import 'package:example/core/network/models/order_detail_model/order_detail_model.dart';
 import 'package:example/core/network/models/order_model/order_model.dart';
+import 'package:example/cubits/billing_detail/billing_detail_cubit.dart';
+import 'package:example/cubits/billing_detail/billing_detail_state.dart';
 import 'package:example/cubits/order/order_cubit.dart';
 import 'package:example/cubits/order/order_state.dart';
 import 'package:example/cubits/product/product_cubit.dart';
@@ -25,6 +27,13 @@ class Checkout extends StatefulWidget {
 }
 
 class _CheckoutState extends State<Checkout> {
+  late final userState;
+  @override
+  void initState() {
+    super.initState();
+    userState = context.read<AuthCubit>().state;
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<OrderCubit, OrderState>(
@@ -40,103 +49,130 @@ class _CheckoutState extends State<Checkout> {
           toast.show(context);
         }
       },
-      child: BlocBuilder<CartCubit, CartState>(
-        builder: (context, cartState) {
-          Widget checkoutContent;
+      child: BlocProvider(
+        create: (_) => BillingDetailCubit(),
+        child: BlocBuilder<CartCubit, CartState>(
+          builder: (context, cartState) {
+            Widget checkoutContent;
 
-          if (cartState is CartLoading) {
-            checkoutContent = const Center(child: CircularProgressIndicator());
-          } else if (cartState is CartLoaded) {
-            checkoutContent = Column(
-              children: [
-                context.emptySizedHeightBoxNormal,
-                AddressBoxModal(
-                  name: "Theresa Webb",
-                  address: "3517 W. Gray St. Utica, Pennsylvania 57867",
-                  email: "willie.jennings@example.com",
-                  onTap: () {
-                    AutoRouter.of(context).push(AddressesViewRoute());
-                  },
-                  phone: "(480) 555-0103",
-                ),
-                context.emptySizedHeightBoxNormal,
-                PaymentMethodBoxModal(
-                  onTap: () {
-                    AutoRouter.of(context)
-                        .push(const PaymentMethodsViewRoute());
-                  },
-                  text: "Master card Ending *****23",
-                ),
-                context.emptySizedHeightBoxNormal,
-                CheckoutListLayout(
-                  items: cartState.cart.map((item) {
-                    return ProductBoxModal(
-                      imagePath: item.product!.image,
-                      name: item.product!.name,
-                      price: item.product!.price,
-                      quantity: item.quantity,
-                      size: item.size,
-                      onTap: () {
-                        context
-                            .read<ProductCubit>()
-                            .changeProduct(item.product!);
-                        AutoRouter.of(context).push(const ProductViewRoute());
-                      },
-                    );
-                  }).toList(),
-                ),
-                context.emptySizedHeightBoxNormal,
-                CustomButton(
-                  onPressed: widget.buttonCallBack ??
-                      () async {
-                        final cartState = context.read<CartCubit>().state;
-                        final userState = context.read<AuthCubit>().state;
-                        if (cartState is CartLoaded) {
-                          if (userState is AuthAuthenticated) {
-                            final cartItems = cartState.cart;
-                            if (cartItems.isNotEmpty) {
-                              final order = Order(
-                                user_id: userState.user!.id,
-                                total_price: await context
-                                    .read<CartCubit>()
-                                    .getCartTotal(userState.user.id),
-                              );
-                              final orderDetails = cartItems.map((item) {
-                                return OrderDetail(
-                                  product_id: item.productId,
-                                  quantity: item.quantity,
-                                  unit_price: item.unitPrice,
-                                  color: item.color,
-                                  size: item.size,
+            if (cartState is CartLoading) {
+              checkoutContent =
+                  const Center(child: CircularProgressIndicator());
+            } else if (cartState is CartLoaded) {
+              context.read<BillingDetailCubit>().getBillingDetail(
+                  userId: userState.user!.id, isDefault: true);
+              checkoutContent = Column(
+                children: [
+                  context.emptySizedHeightBoxNormal,
+                  BlocBuilder<BillingDetailCubit, BillingDetailState>(
+                    builder: (context, billingDetailState) {
+                      if (billingDetailState is BillingDetailLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (billingDetailState is BillingDetailLoaded) {
+                        final billingDetail = billingDetailState.billingDetail;
+
+                        return AddressBoxModal(
+                          name: billingDetail.first.city!,
+                          address: billingDetail.first.address!,
+                          email: billingDetail.first.emailAddress!,
+                          phone: billingDetail.first.phoneNumber!,
+                          onTap: () {
+                            AutoRouter.of(context).push(AddressesViewRoute());
+                          },
+                        );
+                      } else if (billingDetailState is BillingDetailError) {
+                        return Text('Error: ${billingDetailState.message}');
+                      } else {
+                        return AddressBoxModal(
+                          name: "No Data",
+                          address: "Please update your address",
+                          email: "No Email",
+                          phone: "No Phone",
+                          onTap: () {
+                            AutoRouter.of(context).push(AddressesViewRoute());
+                          },
+                        );
+                      }
+                    },
+                  ),
+                  context.emptySizedHeightBoxNormal,
+                  PaymentMethodBoxModal(
+                    onTap: () {
+                      AutoRouter.of(context)
+                          .push(const PaymentMethodsViewRoute());
+                    },
+                    text: "Master card Ending *****23",
+                  ),
+                  context.emptySizedHeightBoxNormal,
+                  CheckoutListLayout(
+                    items: cartState.cart.map((item) {
+                      return ProductBoxModal(
+                        imagePath: item.product!.image,
+                        name: item.product!.name,
+                        price: item.product!.price,
+                        quantity: item.quantity,
+                        size: item.size,
+                        onTap: () {
+                          context
+                              .read<ProductCubit>()
+                              .changeProduct(item.product!);
+                          AutoRouter.of(context).push(const ProductViewRoute());
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  context.emptySizedHeightBoxNormal,
+                  CustomButton(
+                    onPressed: widget.buttonCallBack ??
+                        () async {
+                          final cartState = context.read<CartCubit>().state;
+                          if (cartState is CartLoaded) {
+                            if (userState is AuthAuthenticated) {
+                              final cartItems = cartState.cart;
+                              if (cartItems.isNotEmpty) {
+                                final order = Order(
+                                  user_id: userState.user!.id,
+                                  total_price: await context
+                                      .read<CartCubit>()
+                                      .getCartTotal(userState.user.id),
                                 );
-                              }).toList();
+                                final orderDetails = cartItems.map((item) {
+                                  return OrderDetail(
+                                    product_id: item.productId,
+                                    quantity: item.quantity,
+                                    unit_price: item.unitPrice,
+                                    color: item.color,
+                                    size: item.size,
+                                  );
+                                }).toList();
 
-                              context
-                                  .read<OrderCubit>()
-                                  .postOrder(order, orderDetails);
+                                context
+                                    .read<OrderCubit>()
+                                    .postOrder(order, orderDetails);
+                              }
                             }
                           }
-                        }
-                      },
-                  height: 50,
-                  text: "Order Now",
+                        },
+                    height: 50,
+                    text: "Order Now",
+                  ),
+                ],
+              );
+            } else {
+              checkoutContent = SizedBox(
+                height: 100,
+                child: Center(
+                  child: HeadText(
+                    text: "Cart is Empty",
+                    color: ColorConstant.instance.neutral1,
+                  ),
                 ),
-              ],
-            );
-          } else {
-            checkoutContent = SizedBox(
-              height: 100,
-              child: Center(
-                child: HeadText(
-                  text: "Cart is Empty",
-                  color: ColorConstant.instance.neutral1,
-                ),
-              ),
-            );
-          }
+              );
+            }
 
-          return SingleChildScrollView(child: checkoutContent);
-        },
+            return SingleChildScrollView(child: checkoutContent);
+          },
+        ),
       ),
     );
   }
