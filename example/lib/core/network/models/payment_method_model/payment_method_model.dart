1c3d073a2e67cf9a3c2eeeee69317248e145b3ba
KaanCL <kaancerkez34@outlook.com>
2025-04-11 16:14:12 +0300

    add ✨ : Created a model class for PaymentMethod.

---
+++ example/lib/core/network/models/payment_method_model/payment_method_model.dart
@@ -0,0 +1,62 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'payment_method_model.g.dart';
+
+@JsonSerializable()
+class PaymentMethod {
+  final int? id;
+  final String? created_at;
+  final String name;
+  final String card_last4;
+  final String card_brand;
+  final String card_expiry_month;
+  final int card_expiry_year;
+  final String token;
+  final bool is_default;
+  final String user_id;
+  final String card_first4;
+
+  PaymentMethod({
+    this.id,
+    this.created_at,
+    required this.name,
+    required this.card_last4,
+    required this.card_brand,
+    required this.card_expiry_month,
+    required this.card_expiry_year,
+    required this.token,
+    required this.is_default,
+    required this.user_id,
+    required this.card_first4,
+  });
+
+  factory PaymentMethod.fromJson(Map<String, dynamic> json) {
+    return PaymentMethod(
+      id: json['id'],
+      created_at: json['created_at'],
+      name: json['name'],
+      card_last4: json['card_last4'],
+      card_brand: json['card_brand'],
+      card_expiry_month: json['card_expiry_month'],
+      card_expiry_year: json['card_expiry_year'],
+      token: json['token'],
+      is_default: json['is_default'],
+      user_id: json['user_id'],
+      card_first4: json['card_first4'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'card_last4': card_last4,
+      'card_brand': card_brand,
+      'card_expiry_month': card_expiry_month,
+      'card_expiry_year': card_expiry_year,
+      'token': token,
+      'is_default': is_default,
+      'user_id': user_id,
+      'card_first4': card_first4,
+    };
+  }
+}
---
+++ example/lib/core/network/models/payment_method_model/payment_method_model.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment_method_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PaymentMethod _$PaymentMethodFromJson(Map<String, dynamic> json) =>
+    PaymentMethod(
+      id: (json['id'] as num?)?.toInt(),
+      created_at: json['created_at'] as String?,
+      name: json['name'] as String,
+      card_last4: json['card_last4'] as String,
+      card_brand: json['card_brand'] as String,
+      card_expiry_month: json['card_expiry_month'] as String,
+      card_expiry_year: (json['card_expiry_year'] as num).toInt(),
+      token: json['token'] as String,
+      is_default: json['is_default'] as bool,
+      user_id: json['user_id'] as String,
+      card_first4: json['card_first4'] as String,
+    );
+
+Map<String, dynamic> _$PaymentMethodToJson(PaymentMethod instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'created_at': instance.created_at,
+      'name': instance.name,
+      'card_last4': instance.card_last4,
+      'card_brand': instance.card_brand,
+      'card_expiry_month': instance.card_expiry_month,
+      'card_expiry_year': instance.card_expiry_year,
+      'token': instance.token,
+      'is_default': instance.is_default,
+      'user_id': instance.user_id,
+      'card_first4': instance.card_first4,
+    };
