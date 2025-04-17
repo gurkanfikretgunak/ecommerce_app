3faf53d91ba8055544e068fd6f86d9621316c391
KaanCL <kaancerkez34@outlook.com>
2025-04-17 14:42:06 +0300

    feat 🆕 : FilterView dynamically created FilterFormLabel by listening to SearchCubit.

--- example/lib/views/filter/filter_view.dart
+++ example/lib/views/filter/filter_view.dart
@@ -1,4 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:example/core/network/models/filter_model/filter_model.dart';
+import 'package:example/cubits/search_cubit/search_cubit.dart';
+import 'package:example/cubits/search_cubit/search_state.dart';
 import 'package:example/views/mainpage/models/bottom_navigation_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,31 +16,78 @@ class FilterView extends StatefulWidget {
 }
 
 class _FilterViewState extends State<FilterView> {
+  final TextEditingController categorieController = TextEditingController();
+
+  final List<Color> colors = [
+    Colors.brown,
+    Colors.grey,
+    Colors.blue,
+    Colors.pinkAccent,
+    Colors.green,
+    Colors.yellow,
+  ];
+
+  final List<String> sizes = ["XS", "S", "M", "L", "XL"];
+
+  final List<String> categories = [
+    'men',
+    'women',
+    'kids',
+    'bags',
+    'shoes',
+    'accessories',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            const FilterFormLabel(),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: CustomButton(
-                  onPressed: () {
-                    context.read<BottomNavigationCubit>().setPage(3);
-                    /*Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SearchView()));*/
-                  },
-                  height: 50,
-                  text: "Apply Filter"),
-            ),
-          ],
-        ),
+      body: BlocBuilder<SearchCubit, SearchState>(
+        builder: (context, state) {
+          if (state is SearchApply) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    FilterFormLabel(
+                      categorieController: categorieController,
+                      selectedColor: state.filter.color,
+                      categories: categories,
+                      colors: colors,
+                      sizes: sizes,
+                      selectedSize: state.filter.size,
+                      onSizeSelected: (size) {
+                        context.read<SearchCubit>().changeSize(size);
+                      },
+                      onColorSelected: (color) {
+                        context.read<SearchCubit>().changeColor(color);
+                      },
+                      onPriceChanged: (price) {
+                        context.read<SearchCubit>().changePrice(price);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                      height: 50,
+                      text: "Apply Filter",
+                      onPressed: () {
+                        context.read<BottomNavigationCubit>().setPage(3);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
-    ));
+    );
   }
 }
