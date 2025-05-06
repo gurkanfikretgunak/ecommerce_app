ab1f05b303a81cac1280f009798959b50a14bc09
KaanCL <kaancerkez34@outlook.com>
2025-05-06 11:31:39 +0300

    feat 🆕: PopUpSaleBanner and CategoriesRowLayout dimensions have been updated to be responsive across different screen sizes.

--- lib/src/widgets/layout/categories_row_layout/categories_row_layout.dart
+++ lib/src/widgets/layout/categories_row_layout/categories_row_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class CategoriesRowLayout extends StatelessWidget {
   final List<Widget> items;
@@ -10,6 +11,16 @@ class CategoriesRowLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    double calculatedMargin;
+
+    if (kIsWeb) {
+      calculatedMargin = screenWidth * 0.1;
+    } else {
+      calculatedMargin = screenWidth * 0.05;
+    }
+
     return SizedBox(
       height: height ?? 100,
       child: ListView.builder(
@@ -18,7 +29,7 @@ class CategoriesRowLayout extends StatelessWidget {
           itemBuilder: (context, index) {
             return Container(
               width: itemsWidth ?? 75,
-              margin: const EdgeInsets.only(right: 20),
+              margin: EdgeInsets.only(right: calculatedMargin),
               child: items[index],
             );
           }),
